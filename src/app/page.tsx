9b31c73b9bfe4065e42d5abbbd1d079d248f3aa5
Darth Varg <cbvargas123@gmail.com>
2025-03-20 12:57:56 -0700

    daisy ui and other deps brought in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <button className="d-btn d-btn-primary">Click Me!</button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
